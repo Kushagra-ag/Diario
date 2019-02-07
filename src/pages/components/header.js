510edf22953cbc23c8daa9958f63911074635dc3
kushagra-ag <kushag44@yahoo.com>
2019-02-07 13:02:20 +0530

    logo

--- src/pages/components/header.js
+++ src/pages/components/header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from '../../images/logo.png';
 
 
 export const Header = (props) => {
     return(
             <nav className="navbar navbar-expand-md bg-theme navbar-dark">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand" href="#"><img src={logo}/></a>
                 <button className="btn btn-link btn-outline-light d-inline-block ml-auto" type="button" id="sidebarCollapse">
                 <span className="navbar-toggler-icon btn-sm"></span>
                     <span></span>
