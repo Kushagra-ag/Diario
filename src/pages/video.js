c8bcf5b6920daa37c15fdc5f90670272754cb7c8
kushagra-ag <kushag44@yahoo.com>
2019-03-28 02:51:46 +0530

    video 3

--- src/pages/video.js
+++ src/pages/video.js
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
 
+const KEY = "AIzaSyAdwEtSRKZTBk3-W3ygZvVcawIebzDXzh8";
+const BASEURL = "https://www.googleapis.com/youtube/v3/search";
+
 class Video extends Component {
 
     constructor(props) {
         super(props);
 
         this.state = {
-
+            query: "",
+            error: ""
         }
+
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onSubmit() {
+
+        if(this.state.query === "") {
+            this.setState({
+                ...this.state,
+                error: "Input field is empty"
+            });
+        }
+
+        let url = `${BASEURL}?q=${this.state.query}&part=snippet&key=${KEY}&maxResults=2&type=video&videoEmbeddable=true`;
+
+        fetch(url)
+		.then(results => results.json())
+		.then(data => {
+
+			console.log(data);
+			//console.log(url);
+
+			let videoid = data.items.map(video => video.id.videoId);
+			console.log(videoid);
+
+			returnFrames(videoid);
+		})
+    }
+
+    onChange(e) {
+        this.setState({
+            ...this.state,
+            query: e.target.value
+        })
+    }
+
+    render() {
+
+
+        return(
+
+        );
     }
 }
 
