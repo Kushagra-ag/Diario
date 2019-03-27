6e61b154c4dac2558d3562c572ff0015969d9619
kushagra-ag <kushag44@yahoo.com>
2019-03-28 02:18:38 +0530

    video 2

---
+++ src/pages/video.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Video extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+}
+
+export default Video;
